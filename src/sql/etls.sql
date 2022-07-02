9b0990b20c1d4718ac71a6809a315d4b66eb27a1
fnsalinas <fabio.salinas1982@gmail.com>
2022-07-02 20:49:34 +0000

    Adding some SQl updates

--- src/sql/etls.sql
+++ src/sql/etls.sql
@@ -45,3 +45,23 @@ WHERE nombre_cliente = '{client_name}' AND order_date BETWEEN '{start_date}' AND
 SELECT *
 FROM public.etl_clients_resume
 WHERE order_date BETWEEN '{start_date}' AND '{end_date}';
+
+-- RECREATE_DATE_DEPARTMENT_CATEGORY_PRODUCT_RESUME
+DROP TABLE IF EXISTS public.date_department_category_product_resume;
+CREATE TABLE public.date_department_category_product_resume as
+SELECT
+	EXTRACT(YEAR FROM ord.order_date::DATE) AS order_year,
+	EXTRACT(MONTH FROM ord.order_date::DATE) AS order_month,
+	de.department_name,
+	ca.category_name,
+	CONCAT('(', oi.order_item_product_id, ') ', pr.product_name) AS product,
+	SUM(oi.order_item_quantity) AS quantity,
+	ROUND(SUM(oi.order_item_subtotal)::NUMERIC, 0) AS subtotal
+FROM public.order_items oi
+	LEFT JOIN public.products pr ON oi.order_item_product_id = pr.product_id
+	LEFT JOIN public.orders ord ON oi.order_item_order_id = ord.order_id
+	LEFT JOIN public.categories ca ON pr.product_category_id = ca.category_id
+	LEFT JOIN public.departments de ON ca.category_department_id = de.department_id
+WHERE pr.product_id IS NOT NULL
+GROUP BY 1, 2, 3, 4, 5
+ORDER BY 7 DESC, 6 DESC;
