180cef6478938fc648449a2f8760eb588d1806e5
fnsalinas <fabio.salinas1982@gmail.com>
2022-07-02 20:51:36 +0000

    Adding final updates

--- src/etl_exec/postgres_execution.py
+++ src/etl_exec/postgres_execution.py
@@ -1,18 +1,81 @@
-from typing import Dict, Any
+# %%
+import sys
+import json
+import datetime
+from typing import List, Tuple, Dict, Any
 from sql.sql_reader import getSQLDict
 from common.files_reader import read_json
 from connection.connection import Database
 
-def run_insert_clients_resume() -> str:
+# %%
+def run_insert_clients_resume(debug: bool = False) -> str:
     """
     Executes the insert_clients_resume.sql script.
     Returns:
         A string with the number of rows affected.
     """
     sql_dict: Dict[str, str] = getSQLDict("/home/FABIO/etl/src/sql/etls.sql")
+    if debug: print(f"sql_dict: {sql_dict}\n\n")
     connection_data: Dict[str, Any] = read_json("/home/FABIO/etl/private/postgres_data.json")
+    if debug: print(f"connection_data: {connection_data}\n\n")
     db = Database(**connection_data)
     sql: str = sql_dict.get("CLIENTS_RESUME_INSERT")
+    if debug: print(f"sql: {sql}\n\n")
     rows_affected = db.insert(sql)
     print(rows_affected)
     return rows_affected
+
+def read_data(sql_name: str, sql_vars: Dict[str, str], debug: bool = False) -> List[Dict[str, Any]]:
+    """
+    Executes the sql_name script.
+    Args:
+        sql_name: The name of the sql script to execute.
+        sql_vars: The variables to replace in the sql script.
+        debug: If True, prints the sql script and the variables.
+    Returns:
+        A list of dictionaries with the data.
+    """
+    if isinstance(sql_vars, str):
+        if debug: print(f"isinstance(sql_vars, str) = {isinstance(sql_vars, str)}")
+        sql_vars: Dict[str, str] = json.loads(sql_vars.replace("'", '"'))
+    
+    sql_dict: Dict[str, str] = getSQLDict("/home/FABIO/etl/src/sql/etls.sql")
+    if debug: print(f"sql_dict: {sql_dict}\n\n")
+
+    connection_data: Dict[str, Any] = read_json("/home/FABIO/etl/private/postgres_data.json")
+    if debug: print(f"connection_data: {connection_data}\n\n")
+
+    db = Database(**connection_data)
+    sql: str = sql_dict.get(sql_name).format(**sql_vars)
+    if debug: print(f"sql: {sql}\n\n")
+
+    select: List[str] = [x for x in sql.split("\n") if "SELECT" in x.upper()]
+    if select:
+        fields: List[str] = select[0].split(",")
+    else:
+        fields: List[str] = []
+
+    columns: List[str] = [x.split(" ")[1].strip() if "SELECT" in x.upper() else x.strip() for x in fields]
+
+    data_list = db.read(sql)
+    if debug: print(f"data_list: {data_list}\n\n")
+
+    output_rows: List[Dict[str, Any]] = []
+    timestamp: str = datetime.datetime.now().isoformat()
+    for n, row in enumerate(data_list):
+        row_dict: Dict[str, Any] = {"timestamp": timestamp, "result_no": n+1}
+        for index, column_data in enumerate(row):
+            if isinstance(column_data, datetime.date):
+                column_data = column_data.strftime("%Y-%m-%d")
+            row_dict[columns[index]] = column_data
+        
+        output_rows.append(row_dict)
+    
+    if debug: print(f"output_rows: {output_rows}\n\n")
+
+    return output_rows
+
+if __name__=="__main__":
+    # run_insert_clients_resume(debug = True)
+    rows = read_data(sys.argv[1], sys.argv[2], True)
+    rows
--- src/sql/etls.sql
+++ src/sql/etls.sql
@@ -30,3 +30,38 @@ select t.* from (
 ) as t
 	left join public.etl_clients_resume e on e.order_date = t.order_date and e.customer_city = t.customer_city and e.nombre_cliente = t.nombre_cliente
 where e.order_date is null and e.customer_city is null and e.nombre_cliente is null;
+
+-- CLIENTS_RESUME_QUERY_PER_NAME
+SELECT order_date, cantidad_productos_vendidos, montos_vendidos
+FROM public.etl_clients_resume
+WHERE nombre_cliente = '{client_name}';
+
+-- CLIENTS_RESUME_QUERY_PER_NAME_DATE
+SELECT order_date, cantidad_productos_vendidos, montos_vendidos
+FROM public.etl_clients_resume
+WHERE nombre_cliente = '{client_name}' AND order_date BETWEEN '{start_date}' AND '{end_date}';
+
+-- CLIENTS_RESUME_QUERY_PER_DATE
+SELECT *
+FROM public.etl_clients_resume
+WHERE order_date BETWEEN '{start_date}' AND '{end_date}';
+
+-- RECREATE_DATE_DEPARTMENT_CATEGORY_PRODUCT_RESUME
+DROP TABLE IF EXISTS public.date_department_category_product_resume;
+CREATE TABLE public.date_department_category_product_resume as
+SELECT
+	EXTRACT(YEAR FROM ord.order_date::DATE) AS order_year,
+	EXTRACT(MONTH FROM ord.order_date::DATE) AS order_month,
+	de.department_name,
+	ca.category_name,
+	CONCAT('(', oi.order_item_product_id, ') ', pr.product_name) AS product,
+	SUM(oi.order_item_quantity) AS quantity,
+	ROUND(SUM(oi.order_item_subtotal)::NUMERIC, 0) AS subtotal
+FROM public.order_items oi
+	LEFT JOIN public.products pr ON oi.order_item_product_id = pr.product_id
+	LEFT JOIN public.orders ord ON oi.order_item_order_id = ord.order_id
+	LEFT JOIN public.categories ca ON pr.product_category_id = ca.category_id
+	LEFT JOIN public.departments de ON ca.category_department_id = de.department_id
+WHERE pr.product_id IS NOT NULL
+GROUP BY 1, 2, 3, 4, 5
+ORDER BY 7 DESC, 6 DESC;
